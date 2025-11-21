e2f3e805b04fa88909259531b132debb2c2856c9
osama mohamoud <wwwhdosama@gmail.com>
2025-11-21 16:02:38 +0200

    Initial commit: 2025-11-21 16:02:37

---
+++ clsDate.h
@@ -0,0 +1,140 @@
+#include<iostream>
+using namespace  std;
+// Abstract class(contract)
+class clsDateFunctions {
+    virtual string numberToText(int num)=0;
+    virtual  bool isLeap(short y)=0;
+    virtual short numberOFDays(short y)=0;
+    virtual int numberOfHours(short y)=0;
+    virtual  int numberOfMin(int y)=0;
+    virtual int numberOfSeconds(int y)=0;
+    virtual  short NumberOfDaysInMonth(short y, short m)=0;
+    virtual  int hoursInMonth(short y, short m)=0;
+    virtual  int MinInMonth(short y, short m)=0;
+    virtual  int secondsInMonth(short y, short m)=0;
+     virtual string FullDate(short y, short m, short d)=0;
+    virtual short DayOrder(short year, short month, short day)=0;
+    virtual  string DayName(short dayOrder )=0;
+
+
+
+};
+
+class clsDate : private clsDateFunctions  {
+
+//p1
+    string numberToText(int num)
+    {
+        if (num == 0)
+            return "";
+        if (num >= 1 && num <= 19)
+        {
+            string arr[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thriteen", "fourteen", "fivety", "sixteen", "seventeen", "eigtheen", "ninetheen"};
+
+            return arr[num] + " ";
+        }
+
+        if (num >= 20 && num <= 99) //(20,30,40,50,60,70,80,90)
+        {
+            // 33 --> 30+3 --> 30->(20-90) , 3-->(1-19)
+            string arr[] = {"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+            return arr[num / 10] + " " + numberToText(num % 10);
+        }
+
+        if (num >= 100 && num <= 1000)
+        {
+            // 125 --> 100+25
+            string arr[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+            return arr[num / 100] + " hundered " + numberToText(num % 100);
+        }
+
+        if (num >= 1000 && num <= 10000)
+        {
+            // 2500 --> 2000+500
+            string arr[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            return arr[num/1000]+" thousand "+numberToText(num%1000);
+        }
+    }
+
+//p2||3
+    bool isLeap(short y)
+    {
+        // if it divided by 400 --> leap
+        // if it divided by 4 And not divided by 100 --> leap
+        return ((y % 400 == 0)|| (y%4==0 && y%100!=0) );
+    }
+
+// 4
+    short numberOFDays(short y)
+    {
+        return (isLeap(y) == true) ? 366 : 365;
+    }
+
+    int numberOfHours(short y)
+    {
+        return 24 * numberOFDays(y);
+    }
+
+    int numberOfMin(int y)
+    {
+        return numberOfHours(y) * 60;
+    }
+
+    int numberOfSeconds(int y)
+    {
+        return numberOfMin(y) * 60;
+    }
+
+    //5 &6
+    short NumberOfDaysInMonth(short y, short m){
+
+        if (m<1 || 12<m) return 0;
+        // return (m==2)?((isLeap(y)==true)?  29 : 28) : (m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12) ? 31 : 30;
+
+        short arr[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+        return  (m==2)?((isLeap(y)==true)?  29 : 28):arr[m];
+    }
+
+    int hoursInMonth(short y, short m)
+    {
+        return 24 * NumberOfDaysInMonth(y, m);
+    }
+
+    int MinInMonth(short y, short m)
+    {
+        return 60 * hoursInMonth(y, m);
+    }
+
+    int secondsInMonth(short y, short m)
+    {
+        return MinInMonth(y, m) * 60;
+    }
+
+    //7
+    string FullDate(short y, short m, short d)
+    {
+        return (to_string(d) + "/" + to_string(m) + "/" + to_string(y));
+    }
+
+    short DayOrder(short year, short month, short day)
+    {
+        short a = ((14 - month) / 12);
+        short y = year - a;
+        short m = month + 12 * a - 2;
+
+        return (day + y + ((y / 4)) - ((y / 100)) + ((y / 400)) + (((31 * m) / 12))) % 7;
+    }
+
+    string DayName(short dayOrder )
+    {
+        string Day[]={"sun","mon","tue","wed","thu","fri","sat"};
+        return Day[dayOrder];
+    }
+
+    
+
+};
+
+
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "clsDate.h";
+#include<iostream>
+using namespace std;
+int main() {
+    clsDate d1;
+
+}
